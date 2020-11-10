63beac328d6c1a79c6c04dd94ae36c4ddf877da8
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-11-10 19:37:27 +0000

    created tables and added three entries into Store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,27 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# create 3 stores
+store = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+store = Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+store = Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+# output count to the terminal
+puts "Total stores: #{Store.count}"
