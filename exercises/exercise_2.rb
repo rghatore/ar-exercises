505a5067d19bf6f5b8bbcb74ef28a64f29d50fd3
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-11-10 19:53:20 +0000

    updated @store1 which is the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# load store with id 1
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.name = "White Rock"
+@store1.save
+# OR @store1.update(name: "White Rock")
+# puts @store1.name
