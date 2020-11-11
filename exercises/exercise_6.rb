8c8daeb7f73130259167f8cec18745147557c376
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-11-11 04:43:08 +0000

    added store and employee relation and created some employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# one to many relationship
+# add employees for a particular store
+@store1.employees.create(first_name: "Clark", last_name: "Kent", hourly_rate: 44)
+@store1.employees.create(first_name: "Bruce", last_name: "Wayne", hourly_rate: 50)
+@store1.employees.create(first_name: "Diana", last_name: "Prince", hourly_rate: 46)
+@store2.employees.create(first_name: "Jason", last_name: "Todd", hourly_rate: 40)
+@store2.employees.create(first_name: "Michael", last_name: "Schumacher", hourly_rate: 42)
+@store2.employees.create(first_name: "Gal", last_name: "Gadot", hourly_rate: 40)
+@store1.employees.create(first_name: "Naomi", last_name: "Osaka", hourly_rate: 42)
+
